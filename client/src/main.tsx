e53d01aa783294ed225d94f35dfb5edd1991ed30
ferrabled <48551658+ferrabled@users.noreply.github.com>
2024-11-21 17:15:24 +0100

    fix: Noirjs version and toaster fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+import { Toaster } from "./components/ui/toaster.tsx";
 import "./index.css";
 import NoirProvider from "./lib/providers/noir-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <NoirProvider>
+      <Toaster />
       <App />
     </NoirProvider>
   </StrictMode>,
